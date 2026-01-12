d1153a53377aa03b9624ad2c9f64e7f41d0203c6
Elif Çelik <elifcelik.eva@gmail.com>
2026-01-12 20:13:43 +0300

    Initial commit

---
+++ src/main/java/com/controller/DevOpsController.java
@@ -0,0 +1,22 @@
+package com.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping
+public class DevOpsController {
+
+    @GetMapping
+    public String devopsHello(){
+        return "Hello DevOps! " + LocalDateTime.now();
+    }
+
+    @GetMapping("info")
+    public String info(){
+        return "Info : " + LocalDateTime.now();
+    }
+}
